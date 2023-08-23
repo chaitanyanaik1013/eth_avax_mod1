43b5e82dd419a7d59d24a3a6b798c7fc6c36d30c
Chaitanya Naik <naikchaitanyaabc@gmail.com>
2023-08-23 20:36:33 +0530

    Update myContract.sol

--- myContract.sol
+++ myContract.sol
@@ -3,17 +3,17 @@ pragma solidity ^0.8.1;
 
 contract ErrorHandlingContract {
 
-    int mass = 0;
+    int mass; 
+    int acc;
+    function handler(int _mass, int _acc) public pure returns (int, int) {
+        require(_mass > 0,"Mass of the object Cannot be 0 or less.");
 
-    function handler(int _mass, int _acc) public view returns (int) {
-        require(_mass > 0,"Mass of the object Cannot be 0.");
+        assert(_mass > 0 && _mass <= 10);
 
-        assert(_mass == mass);
-
-        if ((_mass * _acc) < 0){
-            revert("Force of the object cannot be negative");
+        if ((_mass * _acc) >= 100){
+            revert("Force of the object is very large");
         }
 
-        return _mass * _acc;
+        return (_mass, _acc);
     }
 }
